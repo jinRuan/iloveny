e3fea36a0e0e1c4cedc82ed3c7ea08a5ee0fbd06
jin ruan <jruan6377@bths.edu>
2014-04-13 01:18:40 -0400

    added stuff

--- Project Files/Level Design + Programming/Runner/js/game/Level.js
+++ Project Files/Level Design + Programming/Runner/js/game/Level.js
@@ -1,3 +1,12 @@
+var generator = ["1","2","A","B"];
+var string = "";
+var i = 0;
+var randomnumber = Math.floor(Math.random() * 4);
+while (i<10){
+    string = string.concat(generator[Math.floor(Math.random() * 4)]);
+    i++;
+};
+
 var level = [
 
     {player_speed:0.02},
@@ -7,81 +16,82 @@ var level = [
     //Obstacles
     {begin_segment:"Some Obstacles"},
 
-   {time:0,     event:"obstacle_frequency",     value:700},
+    {time:0, event:"obstacle_frequency", value:700},
 
-   {time:0,     event:"obstacle_pattern",     value:"12"}, //2A121B21
+    {time:0, event:"obstacle_pattern", value:string}, //2A121B21
 
-   {time:3,    event:"end_segment"},
+    {time:1, event:"end_segment"},
 
 
     //Coins
     {begin_segment:"Coin Line"},
 
-    {time:0,     event:"coin_y",         value:0.75},
+    {time:0, event:"coin_y", value:0.75},
 
-    {time:2,     event:"coin_frequency", value:500},
+    {time:1, event:"coin_frequency", value:1000},
 
-    {time:3,     event:"end_segment"},
-
-/*
-    //Obstacle
-    {begin_segment:"An Obstacle"},
-
-    {time:0,     event:"obstacle_frequency",     value:1},
-
-    {time:0,     event:"obstacle_pattern",     value:"1"},
-
-    {time:1,     event:"end_segment"},
+    {time:2, event:"end_segment"},
 
 
-    //Coin Boxes
-    {begin_segment:"Coin Boxes"},
+    /*
+        //Obstacle
+        {begin_segment:"An Obstacle"},
 
-    {time:0,     event:"coin_y",             value:0.5},
+        {time:0,     event:"obstacle_frequency",     value:1},
 
-    {time:0,     event:"coin_frequency",     value:0},
+        {time:0,     event:"obstacle_pattern",     value:"1"},
 
-    {time:0,     event:"coin_box",           size:2},
-
-    {time:2,     event:"coin_box",          size:4},
-
-    {time: 6,    event:"end_segment"}, 
+        {time:1,     event:"end_segment"},
 
 
-    //Obstacle
-    {begin_segment:"Some More Obstacles"},
+        //Coin Boxes
+        {begin_segment:"Coin Boxes"},
 
-    {time:0,     event:"obstacle_frequency",     value:700},
+        {time:0,     event:"coin_y",             value:0.5},
 
-    {time:0,     event:"obstacle_pattern",     value:"12"},
+        {time:0,     event:"coin_frequency",     value:0},
 
-    {time:3,     event:"end_segment"},
+        {time:0,     event:"coin_box",           size:2},
 
+        {time:2,     event:"coin_box",          size:4},
 
-    //Coin Sinewave
-    {begin_segment:"A Coin Sinewave"},
-
-    {time:0,     event:"coin_frequency",     value:0},
-
-   {time:0,     event:"coin_frequency",     value:200},
-
-   {time:0,     event:"coin_height",     value:0.5},
-
-    {time:0,     event:"coin_sinewave",     frequency:4, amplitude:100},
-
-    {time:4,    event:"end_segment"},
+        {time: 6,    event:"end_segment"},
 
 
-    //Obstacles
-    {begin_segment:"Even More Obstacles"},
+        //Obstacle
+        {begin_segment:"Some More Obstacles"},
 
-   {time:0,     event:"obstacle_frequency",     value:600},
+        {time:0,     event:"obstacle_frequency",     value:700},
 
-   {time:0,     event:"obstacle_pattern",     value:"12A121B21"},
+        {time:0,     event:"obstacle_pattern",     value:"12"},
 
-   {time:20,    event:"end_segment"},
+        {time:3,     event:"end_segment"},
 
-*/
+
+        //Coin Sinewave
+        {begin_segment:"A Coin Sinewave"},
+
+        {time:0,     event:"coin_frequency",     value:0},
+
+       {time:0,     event:"coin_frequency",     value:200},
+
+       {time:0,     event:"coin_height",     value:0.5},
+
+        {time:0,     event:"coin_sinewave",     frequency:4, amplitude:100},
+
+        {time:4,    event:"end_segment"},
+
+
+        //Obstacles
+        {begin_segment:"Even More Obstacles"},
+
+       {time:0,     event:"obstacle_frequency",     value:600},
+
+       {time:0,     event:"obstacle_pattern",     value:"12A121B21"},
+
+       {time:20,    event:"end_segment"},
+
+    */
     // The End
     {begin_segment:"END"},
     {time:5,     event:"game_finished"}
